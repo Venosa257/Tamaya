ee60d7804f72842a66376d6c7bf27da3f89e23e2
RagilPrasetya <ragil10rpl2@gmail.com>
2023-10-21 20:57:57 +0700

    z

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
