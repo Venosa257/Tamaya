74d0f18cddc05b008598f2bc49491c758698edea
Venosa257 <venosa.arrumi@gmail.com>
2023-10-21 21:19:02 +0700

    ganti

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->middleware('auth');
 
 Route::get('/login',[LoginController::class,'index'])->middleware('guest')->name('login');
 Route::post('/login', [LoginController::class,'authenticate']);
