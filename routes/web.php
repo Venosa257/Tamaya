a2b2617cdfeee715a806c6a2994c896e58f33a9f
RagilPrasetya <ragil10rpl2@gmail.com>
2023-10-21 20:19:13 +0700

    pull en ven

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index']);
 
 Route::get('/login',[LoginController::class,'index'])->middleware('guest');
 Route::get('/register',[RegisterController::class,'create']);
