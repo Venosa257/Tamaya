a9660beea5606e76c1b6e89914a45b6a5dc6af2b
RagilPrasetya <ragil10rpl2@gmail.com>
2023-10-21 22:08:16 +0700

    profile

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,5 @@ Route::post('/login', [LoginController::class,'authenticate']);
 
 Route::get('/register',[RegisterController::class,'create']);
 Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/profile' ,[ProfileController::class,'profile']);
