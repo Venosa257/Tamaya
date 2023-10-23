248f2f5b4a74acc279a9d2b9bea8235d51076bce
RagilPrasetya <ragil10rpl2@gmail.com>
2023-10-23 20:47:16 +0700

    profile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -3,35 +3,39 @@
 
 <div class="position-relative">
     <div class="">
-        <div class="card mx-auto p-2 m-3">
-            <div class="text-center">
-                <p class="fs-2 fw-semibold">zzzzzzzzzzzzzzzzzzzz</p>
-                <p></p>
+        <div class="card mx-auto p-2 m-4 gradient-custom">
+            <div class="text text-start">
+                <p class="fs-2 fw-semibold m-0">Ragil Prasetya Putra Bima</p>
+                <p class="fs-5 light m-0">Tambahkan kredensial profil</p>
+                <p class="fw-5 light ">0 Follower • 0 Following</p>
             </div>
-            <div class="photo">
-                <img src="img/ishimi.jpeg" class="rounded-circle" style="width: 30%" alt="">
+            <div class="photo mt-1">
+                <img src="img/ishimi.jpeg" class="rounded-circle" style="width: 18%" alt="">
+
+            </div>
+            <div class="text-fixed mt-3 ps-3">
+                <p class="fw-5 light">Tulis deskripsi tentang anda</p>
+            </div>
+            <div class="card-header mt-2">
+                <ul class="nav nav-tabs card-header-tabs d-flex justify-content-center">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="true" href="#">Profile</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="true" href="#">Jawaban</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="true" href="#">Pertanyaan</a>
+                  </li>
+
+                </ul>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
             </div>
         </div>
-        <div class="card mx-auto">
-            <div class="card-header">
-              <ul class="nav nav-tabs card-header-tabs d-flex justify-content-center">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="true" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-              </ul>
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-    </div>
 </div>
 @endsection
